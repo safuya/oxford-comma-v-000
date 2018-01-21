68f2968500916eca974fce809f2f1d4056379c46
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 06:58:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   array.each do |item, index|
     sentence = ""
     if item == array[-1] && array.size > 1
-      sentence += "and #{item}"
+      sentence = "#{sentence} and #{item}"
       return sentence
     elsif item == array[-1]
       return array[0]
