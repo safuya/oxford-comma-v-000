0e0376914b32988063e4465d8704ba2d93d23f58
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 07:07:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,15 @@ require 'pry'
 def oxford_comma(array)
   sentence = ""
   array.each do |item, index|
-    if item == array[-1] && array.size > 1
-      sentence += " and #{item}"
+    if item == array[-1] && array.size > 2
+      sentence += "and #{item}"
       return sentence
-    elsif item == array[-1]
+    elsif array.size == 2
+      return "#{array[0]} and #{array[1]}"
+    elsif array.size == 1
       return array[0]
     end
 
-    sentence += item
+    sentence += "#{item}, "
   end
 end
