b44252e1df30a4c66c534a6d7c4d8d3668ac3870
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 07:08:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  sentence = ""
+  array.each do |item, index|
+    if item == array[-1] && array.size > 2
+      sentence += "and #{item}"
+      return sentence
+    elsif array.size == 2
+      return "#{array[0]} and #{array[1]}"
+    elsif array.size == 1
+      return array[0]
+    end
+
+    sentence += "#{item}, "
+  end
 end
